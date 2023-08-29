91f33bcef068d5009f50bc6af3f999bc62b46b0a
Anthony <2060246@cstj.qc.ca>
2023-08-29 17:58:55 -0400

    Demo cours 2

---
+++ src/views/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <DefaultLayout>
+        <p>Page d'accueil</p>
+    </DefaultLayout>
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from '../layouts/DefaultLayout.vue';
+
+
+</script>
+
+<style scoped></style>
