a4a7ef0bb147c0eb6a51a09425cbb08e4111aece
Anthony <2060246@cstj.qc.ca>
2023-09-05 17:54:24 -0400

    added svg

--- src/views/pages/PlanetPage.vue
+++ src/views/pages/PlanetPage.vue
@@ -2,8 +2,45 @@
     <DefaultLayout>
         <div class="pt-5">
             <h1>Planets</h1>
+            <div class="loading" v-if="isLoading">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    style="margin: auto; background: rgb(255, 255, 255); display: block; shape-rendering: auto"
+                    width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+                    <g>
+                        <circle cx="60" cy="50" r="4" fill="#e15b64">
+                            <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1"
+                                begin="-0.67s"></animate>
+                            <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1"
+                                keyTimes="0;0.2;1" begin="-0.67s"></animate>
+                        </circle>
+                        <circle cx="60" cy="50" r="4" fill="#e15b64">
+                            <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1"
+                                begin="-0.33s"></animate>
+                            <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1"
+                                keyTimes="0;0.2;1" begin="-0.33s"></animate>
+                        </circle>
+                        <circle cx="60" cy="50" r="4" fill="#e15b64">
+                            <animate attributeName="cx" repeatCount="indefinite" dur="1s" values="95;35" keyTimes="0;1"
+                                begin="0s"></animate>
+                            <animate attributeName="fill-opacity" repeatCount="indefinite" dur="1s" values="0;1;1"
+                                keyTimes="0;0.2;1" begin="0s"></animate>
+                        </circle>
+                    </g>
+                    <g transform="translate(-15 0)">
+                        <path d="M50 50L20 50A30 30 0 0 0 80 50Z" fill="#f8b26a" transform="rotate(90 50 50)"></path>
+                        <path d="M50 50L20 50A30 30 0 0 0 80 50Z" fill="#f8b26a">
+                            <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s"
+                                values="0 50 50;45 50 50;0 50 50" keyTimes="0;0.5;1"></animateTransform>
+                        </path>
+                        <path d="M50 50L20 50A30 30 0 0 1 80 50Z" fill="#f8b26a">
+                            <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s"
+                                values="0 50 50;-45 50 50;0 50 50" keyTimes="0;0.5;1"></animateTransform>
+                        </path>
+                    </g>
+                </svg>
+            </div>
 
-            <div class="col-12">
+            <div class="col-12" v-else>
                 <div class="row">
 
                     <div class="col-md-4 my-2" v-for="(planet, index) of planets" :key="index">
@@ -12,7 +49,7 @@
                                 <h4>{{ planet.name }}</h4>
                             </div>
                             <div class="text-center card-body">
-                                <img :src="planet.icon">
+                                <img :src="planet.icon" class="planet">
                             </div>
                         </div>
                     </div>
@@ -32,10 +69,17 @@ import { Planet } from '@/models/Planet'
 const planetRepository = new PlanetRepository()
 const planets = ref<Planet[]>([])
 
+const isLoading = ref(true)
 onMounted(async () => {
     planets.value = await planetRepository.retrieveAll()
+    isLoading.value = false
 })
+
 
 </script>
 
-<style scoped></style>
+<style scoped>img.planet {
+    width: 225px;
+    height: auto;
+    margin: auto;
+}</style>
